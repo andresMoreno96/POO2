48995b39d57d2fa2abc7fa6a6a14057f384f3978
Simon Walther <simon.walt@hotmail.com>
2020-05-15 13:53:35 +0200

    add humanoid subclasses

---
+++ buffy/Buffy.hpp
@@ -0,0 +1,14 @@
+//
+// Created by simon on 08.05.20.
+//
+
+#ifndef BUFFY_BUFFY_HPP
+#define BUFFY_BUFFY_HPP
+
+
+class Buffy {
+
+};
+
+
+#endif //BUFFY_BUFFY_HPP
---
+++ buffy/Vampire.hpp
@@ -0,0 +1,14 @@
+//
+// Created by simon on 08.05.20.
+//
+
+#ifndef BUFFY_VAMPIRE_HPP
+#define BUFFY_VAMPIRE_HPP
+
+
+class Vampire {
+
+};
+
+
+#endif //BUFFY_VAMPIRE_HPP
